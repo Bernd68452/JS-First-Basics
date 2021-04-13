87e0d7a8b90e931810c97e8d3fb0a35871a205c3
Bernd68452 <bernd79@gmx.de>
2021-04-13 14:24:35 +0200

    Finsihed 1_5

---
+++ Lev1_5/assets/js/main.js
@@ -0,0 +1,34 @@
+
+
+let Name = "carName";
+console.log("BMW");
+
+let x = "x";
+console.log("150");
+
+let y = "y";
+console.log("50");
+
+let z = "z";
+console.log("200");
+
+let addition_operator = 150 + 50;
+console.log("addition: " + addition_operator);
+
+let firstName = "John"
+console.log("John");
+
+let lastName = "Doe"
+console.log("Doe");
+
+let age = 35
+console.log("35");
+
+
+
+
+
+
+
+
+
