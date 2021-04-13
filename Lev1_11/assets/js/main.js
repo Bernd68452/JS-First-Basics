a563b5f595b1fc3b13624af65ec989ffa2ecc0e7
Bernd68452 <bernd79@gmx.de>
2021-04-13 13:57:03 +0200

    Finished

--- Lev1_11/assets/js/main.js
+++ Lev1_11/assets/js/main.js
@@ -1,3 +1,23 @@
 a = 12;
 b = a * 3;
 console.log(b);
+
+//let Hello World;
+//let greeting = "Hello World!";
+console.log("Hello World!");
+console.log("Anton")
+
+//let name = "Anton";
+//console.log("Anton");
+
+let age = "28";
+console.log("28");
+
+let job = "Trainer";
+console.log("Trainer");
+
+var married = true;
+console.log("married");
+
+let greeting = "Anton ist 28 Jahre alt und ist verheiratet: true"
+console.log("Anton ist 28 Jahre alt und ist verheiratet: true")
